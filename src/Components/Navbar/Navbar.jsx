98df8b47c8c6da93f48cf1542c27b1dc4a26088f
Suprio-Das <priyamd507@gmail.com>
2025-02-24 22:42:23 +0600

    Login Button bg color changed to F9A51A

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex">
                     <label className="input">
                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
-                        <input type="search" className="grow" placeholder="Search" />
+                        <input type="search" className="grow" placeholder="Search your Destination" />
                     </label>
                 </div>
                 <div className="navbar-end">
@@ -35,7 +35,7 @@ const Navbar = () => {
                         <li className="ms-3"><Link>Blog</Link></li>
                         <li className="ms-3"><Link>Contact</Link></li>
                     </ul>
-                    <a className="btn ms-3">Login</a>
+                    <a className="btn ms-3 bg-[#F9A51A]">Login</a>
                 </div>
             </div>
         </div>
