0b205e35d7c638f4b89cfc25b6cd3a17c5fed8b8
Suprio-Das <priyamd507@gmail.com>
2025-02-24 22:27:24 +0600

    Navbar designed with with search field

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,15 +23,19 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+                        <input type="search" className="grow" placeholder="Search" />
+                    </label>
+                </div>
+                <div className="navbar-end">
                     <ul className="menu menu-horizontal px-1">
                         <li><Link>News</Link></li>
                         <li><Link>Destination</Link></li>
                         <li><Link>Blog</Link></li>
                         <li><Link>Contact</Link></li>
                     </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn">Login</a>
                 </div>
             </div>
         </div>
